ffffc6189ad2020a5ae192d63416af5cb6f60a44
ZelAk312 <zelak312@gmail.com>
2016-09-24 20:47:13 +0000

    a

--- main.js
+++ main.js
@@ -2,6 +2,9 @@
 var Musics = require("./src/music.js");
 //msg counter part
 var MsgsCounter = require("./src/msgs.js");
+//new funtions
+var Hear = require("./src/hear.js");
 
 module.exports.Music = Musics;
 module.exports.MsgsCounter = MsgsCounter;
+module.exports.Hear = Hear;
